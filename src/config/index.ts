64a9971d5a048b5caf40f43b541f199e17d84f87
Adam Horodyski <ad.horodyski@gmail.com>
2021-02-22 18:30:04 +0100

    update app name

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import { Env, Config } from "config/models";
 
 export const config: Config = {
     app: {
-        name: "jira-client",
+        name: "server",
         port: 8080,
         env: process.env.NODE_ENV || Env.DEVELOPMENT,
     },
