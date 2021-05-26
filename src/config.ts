676fef0d24cf05b9e02ec93b5faf1372a425df38
Adam Horodyski <ad.horodyski@gmail.com>
2021-05-26 10:56:46 +0200

    shorten config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,6 @@
 import "dotenv/config";
 
 export const config = {
-    app: {
-        name: "server",
-        port: process.env.PORT || 3030,
-        env: process.env.NODE_ENV || "development",
-    },
+    port: process.env.PORT || 3030,
+    env: process.env.NODE_ENV || "development",
 };
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { config } from "@src/config";
 
 (async () => {
     try {
-        await app.listen(config.app.port);
-        console.log(`🚀 Fastify :: port ${config.app.port}`);
+        await app.listen(config.port);
+        console.log(`🚀 Fastify :: port ${config.port}`);
     } catch (err) {
         console.error(err);
         process.exit(1);
